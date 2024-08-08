ce706cf61e3d204ed7933df94b4334c0c802ec7b
Rahi Reyaz <rahiryazdar@gmail.com>
2024-08-08 12:49:05 +0530

    DB setup done

---
+++ src/models/Url.ts
@@ -0,0 +1,29 @@
+import mongoose  , {Document,Model,Schema} from "mongoose";
+
+const urlSchema =new mongoose.Schema({
+    originalUrl :{
+        type:String,
+        required:true,
+        unique:true
+    },
+    shortUrl: {
+        type:String,
+        required:true,
+        unique:true
+    },
+  },
+  {
+    timestamps: true
+        });
+
+
+  export interface IUrl extends Document {
+    originalUrl:string,
+    shortUrl: string
+  }   
+  
+
+  //using this schema ,creating actual model /connection object
+ const Url : Model<IUrl> = mongoose.models.Url || mongoose.model<IUrl>('Url',urlSchema);
+
+ export default Url;
